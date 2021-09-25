a89bbe964a0ed29496294b245718c3c270936c56
Soebb <87156166+Soebb@users.noreply.github.com>
2021-09-25 05:06:52 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 class Config:
 
     BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
-    APP_ID = os.environ.get('APP_ID', None)
+    APP_ID = os.environ.get('API_ID', None)
     API_HASH = os.environ.get('API_HASH', None)
 
     #comma seperated user id of users who are allowed to use
